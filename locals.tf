f621aaea776f4ef6c54fb8ab9e6be1c6d273f5eb
karan <kc95155@gmail.com>
2024-02-26 19:31:17 +0530

    removed locals and remote backend

--- locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  domain_password= data.azurerm_key_vault_secret.domainpw.value
-  address_space={
-    sademo2308="10.8.0.0/16"
-    tfdeploy="10.5.0.0/16"
-    default="10.4.0.0/16"
-  }
-}

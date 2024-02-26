67a654243aa59da01ba8c7187703327d84d811be
karan <kc95155@gmail.com>
2024-02-26 20:21:33 +0530

    fetching backend details from backend.conf

--- provider.tf
+++ provider.tf
@@ -8,13 +8,8 @@ terraform {
   
 }
 
-//backend configuration//
-backend "azurerm" {
-    resource_group_name  = var.backend_rg
-    storage_account_name = var.backend_storage
-    container_name       = var.backend_container_name
-    key                  = var.backend_key
-  }
+//backend configuration accessed from backend.conf during runtime//
+backend "azurerm" {}
 }
 
 provider "azurerm" {

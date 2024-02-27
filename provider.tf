193ae938d89bf1c37fe76ae28f7e94b7eaf61812
karan <kc95155@gmail.com>
2024-02-27 14:36:10 +0530

    removed backend fromprovdier

--- provider.tf
+++ provider.tf
@@ -1,17 +1,11 @@
 terraform {
-  
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
       version = "3.66.0"
     }
-  
 }
-
-//backend configuration accessed from backend.conf during runtime//
-backend "azurerm" {}
 }
-
 provider "azurerm" {
   # Configuration options
     subscription_id = var.subscription_id
@@ -19,16 +13,10 @@ provider "azurerm" {
     client_id = var.client_id
     client_secret = var.client_secret
     features {}
-  
-  
 }
-
-
-
 provider "azuread" {
   # Configuration options
     tenant_id = var.tenant_id
     client_id = var.client_id
-    client_secret = var.client_secret
-    
+    client_secret = var.client_secret  
 }

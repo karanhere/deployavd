4ee8b2a5823a95a8c998f47efcf6bbae55a1a9ef
karan <kc95155@gmail.com>
2024-02-27 18:06:50 +0530

    removed backend

--- provider.tf
+++ provider.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "3.66.0"
+      version = "3.93.0"
     }
 }
-backend "azurerm" {}
+
 }
 provider "azurerm" {
   # Configuration options
